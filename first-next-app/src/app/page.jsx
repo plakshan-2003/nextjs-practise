aed4be4b551c86222e9fbb3db1e5ad4082b0eb14
Pradeep Lakshan <pradeeplakshan2022@gmail.com>
2026-08-23 00:45:28 +0530

    up

--- first-next-app/src/app/page.jsx
+++ first-next-app/src/app/page.jsx
@@ -1,10 +1,16 @@
 import Image from "next/image";
 import styles from "./page.module.css";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <div>
-      <h1>hellow next jss</h1>
+      <ul>
+        <li><Link href="/">Home</Link></li>
+        <li><Link href="/services">Services</Link></li>
+        <li><Link href="/about">About</Link></li>
+        <li><Link href="/contact">Contact</Link></li>
+      </ul>
     </div>
 
 
--- first-next-app/src/app/services/page.jsx
+++ first-next-app/src/app/services/page.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import Link from 'next/link'
 
 function page() {
   return (
     <div>
         <h1>services page</h1>
+        <Link href="/">Back to home</Link>
     </div>
   )
 }
