7d50fa94228dc632811bc79220a59749e9cbdad2
Pradeep Lakshan <pradeeplakshan2022@gmail.com>
2026-08-23 18:39:05 +0530

    css adding

--- first-next-app/src/app/services/page.jsx
+++ first-next-app/src/app/services/page.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import Link from 'next/link'
+import styles from './service.module.css'
 
 function page() {
   return (
     <div>
-        <h1>services page</h1>
+        <h1 className={styles.h1}>services page</h1>
         <Link href="/">Back to home</Link>
     </div>
   )
