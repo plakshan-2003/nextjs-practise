9fee60da6fbb541a4772783de4ab2f5154c98bee
Pradeep Lakshan <pradeeplakshan2022@gmail.com>
2026-08-26 23:44:37 +0530

    shade input

--- first-next-app/src/app/(login)/sign-up/page.jsx
+++ first-next-app/src/app/(login)/sign-up/page.jsx
@@ -1,19 +1,39 @@
 import React from 'react'
+import { Button } from "@/components/ui/button"
+import {
+  Field,
+  FieldDescription,
+  FieldGroup,
+  FieldLabel,
+} from "@/components/ui/field"
+import { Input } from "@/components/ui/input"
 
 function page() {
   return (
     <div>
-      <h1>Sign Up</h1>
-      <form>
-        <ul>
-          <label>Username:</label>  <input type="text" placeholder='Username' />
-          <label>Email:</label>  <input type="email" placeholder='Email' />
-          <label>Password:</label>  <input type="password" placeholder='Password' />
-          <button type='submit'>Sign Up</button>
-        </ul>
-
-
-      </form>
+      <FieldGroup>
+      <Field>
+        <FieldLabel htmlFor="fieldgroup-name">Name</FieldLabel>
+        <Input id="fieldgroup-name" placeholder="Jordan Lee" />
+      </Field>
+      <Field>
+        <FieldLabel htmlFor="fieldgroup-email">Email</FieldLabel>
+        <Input
+          id="fieldgroup-email"
+          type="email"
+          placeholder="name@example.com"
+        />
+        <FieldDescription>
+          We&apos;ll send updates to this address.
+        </FieldDescription>
+      </Field>
+      <Field orientation="horizontal">
+        <Button type="reset" variant="outline">
+          Reset
+        </Button>
+        <Button type="submit">Submit</Button>
+      </Field>
+    </FieldGroup>
     </div>
   )
 }
