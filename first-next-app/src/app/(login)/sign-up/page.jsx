3fd691de1c1804d65599fdf0af859ddc6653eebd
Pradeep Lakshan <pradeeplakshan2022@gmail.com>
2026-08-26 23:37:30 +0530

    form

--- first-next-app/src/app/(login)/sign-up/page.jsx
+++ first-next-app/src/app/(login)/sign-up/page.jsx
@@ -2,7 +2,19 @@ import React from 'react'
 
 function page() {
   return (
-    <div>sign up</div>
+    <div>
+      <h1>Sign Up</h1>
+      <form>
+        <ul>
+          <label>Username:</label>  <input type="text" placeholder='Username' />
+          <label>Email:</label>  <input type="email" placeholder='Email' />
+          <label>Password:</label>  <input type="password" placeholder='Password' />
+          <button type='submit'>Sign Up</button>
+        </ul>
+
+
+      </form>
+    </div>
   )
 }
 
